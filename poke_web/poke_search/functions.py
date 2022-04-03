109d90d63255c767f43e2855f50838b8174fbf11
arcstur <arcstur@gmail.com>
2022-04-02 23:12:32 -0300

    Requisição da PokeAPI Gêneros, Imagens Evoluções

---
+++ poke_web/poke_search/functions.py
@@ -0,0 +1,107 @@
+import requests
+
+def get_poke_json(search_name):
+    url = f'https://pokeapi.co/api/v2/pokemon/{search_name}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        poke_json = response.json()
+        if 'name' in poke_json:
+            if poke_json['name'] == search_name:
+                return poke_json
+
+    return {}
+
+def get_species_json(poke_json):
+    url = poke_json['species']['url']
+    response = requests.get(url)
+    return response.json()
+
+def get_evolution_chain_json(species_json):
+    url = species_json['evolution_chain']['url']
+    response = requests.get(url)
+    return response.json()
+    
+def get_next_evolution(species_name, chain_list):
+    evolves_to_list = []
+
+    for chain_item in chain_list:
+        if chain_item['species']['name'] == species_name:
+            evolves_to_list = [ item['species']['name'] for item in chain_item['evolves_to'] ]
+            return ", ".join(evolves_to_list)
+        
+    for chain_item in chain_list:
+        return_value = find_chain(species_name, chain_item['evolves_to'])
+        if return_value:
+            return return_value
+
+    return ''
+    
+
+
+
+def get_poke_from_json(poke_json):
+    poke = {}
+
+    # Name
+    poke['name'] = poke_json['name'].capitalize()
+
+    # Types
+    poke['type1'] = poke_json['types'][0]['type']['name']
+    poke['type2'] = ''
+
+    if len(poke_json['types']) > 1:
+        poke['type2'] = poke_json['types'][1]['type']['name']
+
+    # Stats
+    poke['stats'] = {}
+    for stat in poke_json['stats']:
+        stat_name = stat['stat']['name'].replace('-', '_')
+        poke['stats'][stat_name] = stat['base_stat']
+
+    # Default Img
+    poke['img'] = poke_json['sprites']['front_default']
+
+    # Shiny Img
+    poke['img_shiny'] = poke_json['sprites']['front_shiny']
+
+    # Species Json
+    species_json = get_species_json(poke_json)
+
+    # Gender
+    gender_rate = int(species_json['gender_rate'])
+
+    poke['genders'] = {}
+    poke['genders']['masc'] = (0 <= gender_rate < 8)
+    poke['genders']['fem'] = (0 < gender_rate <= 8)
+    poke['genders']['genderless'] = (gender_rate == -1)
+
+    # Gender Img
+    if species_json['has_gender_differences']:
+        poke['img_female'] = poke_json['sprites']['front_female']
+
+    # Evolves From
+    evolves_from = species_json['evolves_from_species']
+    if evolves_from:
+        poke['evolves_from'] = evolves_from['name']
+
+    # Evolves to
+    evolution_chain_json = get_evolution_chain_json(species_json)
+    initial_chain_list = [evolution_chain_json['chain']]
+    evolves_to = get_next_evolution(species_json['name'], initial_chain_list)
+    
+    if evolves_to:
+        poke['evolves_to'] = evolves_to
+
+
+
+    return poke
+
+
+def get_pokemon(search_name):
+    poke_json = get_poke_json(search_name)
+
+    if poke_json:
+        return get_poke_from_json(poke_json)
+    else:
+        return ''
--- poke_web/poke_search/views.py
+++ poke_web/poke_search/views.py
@@ -1,44 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+
+from .functions import get_pokemon
 
 # Create your views here.
 
-pokemon_list = [
-    {
-        'name': 'Bulbasaur',
-        'type1': 'grass',
-        'type2': 'poison',
-        'stats': {
-            'hp': 62,
-            'attack': 62,
-            'defense': 32,
-            'special_attack': 33,
-            'special_defense': 17,
-            'speed': 25
-        },
-        'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-        'img_shiny': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png',
-    },
-    {
-        'name': 'Charmander',
-        'type1': 'fire',
-        'type2': '',
-        'stats': {
-            'hp': 62,
-            'attack': 62,
-            'defense': 32,
-            'special_attack': 33,
-            'special_defense': 17,
-            'speed': 25
-        },
-        'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
-        'img_shiny': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/4.png',
-    }
-
-]
-
 def home(request):
+    search_name = ''
+    pokemon_list = []
+
+    if 'search_name' in request.GET:
+        search_name = request.GET['search_name'].lower().strip()
+        poke = get_pokemon(search_name)
+
+        if poke: pokemon_list.append(poke)
+
     context = {
+        'search_name': search_name,
         'pokemon_list': pokemon_list,
     }
     return render(request, 'poke_search/home.html', context)
