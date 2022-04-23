9f66f6263c6f9f5af5e0ea320d60df6c92ff1aa4
arcstur <arcstur@gmail.com>
2022-04-23 13:53:41 -0300

    fix: add localhost to settings

--- poke_web/settings.py
+++ poke_web/settings.py
@@ -21,13 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-5h_c=)xx2r-e-d*jzwpfii(*r7qz294lb5_wcp4*f4ldqby_i('
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['pokencontra.herokuapp.com']
+ALLOWED_HOSTS = [
+    'pokencontra.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
